ee05de792fddd70e16300c6adb540d96ac99d9c4
snakeaid <illia.antypenko@gmail.com>
2023-01-16 16:21:39 +0200

    Add token names parsing

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 'use strict';
 const ccxt = require('ccxt');
 
-(async function() {
+(async function () {
     let gate = new ccxt.gate();
     await gate.loadMarkets();
-    let symbols = gate.symbols;
-    console.dir(symbols, {'maxArrayLength': null});
-}) ();
+    let gateSymbols = gate.symbols;
+})();
+
+const fs = require("fs");
+let symbols = JSON.parse(fs.readFileSync("gate_symbols.rtf", "utf8"));
+
+for (let i = 0; i < symbols.length; i++) {
+    let symbol = symbols[i];
+    symbols[i] = symbol.split('/')[0].split('-')[0];
+}
+console.dir(gateSymbols);
